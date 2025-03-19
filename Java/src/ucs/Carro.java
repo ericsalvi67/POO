6086bbd9faed5ce388befbda714f565598ec79f5
Eric Salvi <ericsalvi67@gmail.com>
2025-03-18 22:00:56 -0300

    conteudo finalizado em aula

--- Java/src/ucs/Carro.java
+++ Java/src/ucs/Carro.java
@@ -2,8 +2,85 @@ package ucs;
 
 public class Carro {
 
-	String nome;
-	String cor;
-	int potencia;
-	String placa;
+	private String nome;
+	private String cor;
+	private int potencia;
+	private String placa;
+	private int velocidade = 0;
+	private static final int VELOCIDADE_MAX = 240;
+	
+	public void setNome(String nome) {
+		
+		if(nome == null || nome.length() < 3) {
+			this.nome = "Indeterminado";
+		}
+		else {
+			this.nome = nome;
+		}
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public int getPotencia() {
+		return potencia;
+	}
+
+	public void setPotencia(int potencia) {
+		if (potencia < 0) {
+			this.potencia = 0;
+		} else {
+			this.potencia = potencia;
+		}
+		this.potencia = potencia;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+	
+	public void acelera() {
+		if (this.velocidade < VELOCIDADE_MAX){
+			this.velocidade += 10;
+		}
+	}
+	
+	public void freia() {
+		if(this.velocidade > 0) {
+			this.velocidade -=10 ;
+		}
+		if(this.velocidade < 0) {
+			this.velocidade = 0;
+		}
+	}
+	
+	public int getVelocidade() {
+		return this.getVelocidade();
+	}
+	
+	
+	public void mostra() {
+		System.out.println("-------------------------------------------");
+		System.out.println("Nome : " + this.getNome());
+		System.out.println("Cor : " + this.getCor());
+		System.out.println("Potencia : " + this.getPotencia());
+		System.out.println("Placa : " + this.getPlaca());
+		System.out.println("Velocidade : " + getVelocidade() + "KM/H");
+		System.out.println("-------------------------------------------");
+	}
+	
+	
 }
--- Java/src/ucs/Principal.java
+++ Java/src/ucs/Principal.java
@@ -1,8 +1,40 @@
 package ucs;
+import java.util.Scanner;
 
 public class Principal {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		
-		System.out.println("Hello World.");
+		Carro c1;
+		Scanner sc = new Scanner(System.in);
+		
+		c1 = new Carro();
+		
+		System.out.println("Informe o nome do carro: ");
+		String s = sc.nextLine();
+		c1.setNome(s);
+		System.out.println("Informe a cor: ");
+		s = sc.nextLine();
+		c1.setCor(s);
+		System.out.println("Informe a potencia: ");
+		int i = sc.nextInt();
+		sc.nextLine();
+		c1.setPotencia(i);
+		System.out.println("Informe a placa: ");
+		s = sc.nextLine();
+		c1.setPlaca(s);
+		
+		System.out.println("Acelera quanto (1 - 20): ");
+		i = sc.nextInt();
+		sc.nextLine();
+		for (int j = 0; j < i; j++) {
+			c1.acelera();
+		}
+		
+			
+		c1.mostra();
+		
+		sc.close();
+
 	}
 }
