669ce4d4e8bec54debb1225ac8d982b6c1669ae6
Éric Salvi <ericsalvi67@hotmail.com>
2025-03-27 23:35:29 -0300

    Exercicio 1

---
+++ Java/Exercicios/Lista2/E1/src/Imobiliaria/Terreno.java
@@ -0,0 +1,21 @@
+package Imobiliaria;
+
+public class Terreno 
+{
+	private int altura;
+	private int largura;
+	
+	
+	public int getAltura() {
+		return altura;
+	}
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+	public int getLargura() {
+		return largura;
+	}
+	public void setLargura(int largura) {
+		this.largura = largura;
+	} 
+}
